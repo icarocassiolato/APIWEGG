dd10fef34629abf09ac64d0e29ff16a29390b62c
Ícaro Cassiolato <icaro.a.m.cassiolato@gmail.com>
2023-07-07 11:29:50 -0300

    Adicionando campos de endereço no imóvel

--- Domain/Entities/Imovel.cs
+++ Domain/Entities/Imovel.cs
@@ -9,5 +9,9 @@
         public decimal? Latitude { get; set; }
         public decimal? Longitude { get; set; }
         public string? CaminhoMatricula { get; set; }
+        public string? Endereco { get; set; }
+        public int? Numero { get; set; }
+        public string? Bairro { get; set; }
+        public string? Complemento { get; set; }
     }
 }
